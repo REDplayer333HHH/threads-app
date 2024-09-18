ac61ba7e3399a909cff995901e938ac065e15c21
Horia Bosoanca <horia.bosoanca@gmail.com>
2024-09-18 22:58:07 +0300

    Added repository pattern

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using threads_app.Data;
 using Microsoft.EntityFrameworkCore;
+using threads_app.Repository;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,8 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<Database>(dbcontextOptionsBuilder => {
     dbcontextOptionsBuilder.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<ThreadRepository>();
 
 var app = builder.Build();
 
--- obj/Debug/net8.0/threads-app.AssemblyInfo.cs
+++ obj/Debug/net8.0/threads-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("threads-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+933e9851d55b2aacfb90503bfa40a692d78d334a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60b3e970e0fedb34ec88bb9f61057bf316743f4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("threads-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("threads-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
