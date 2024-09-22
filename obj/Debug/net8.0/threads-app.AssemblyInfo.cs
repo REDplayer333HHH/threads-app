f0867a0a0392979b814da2d97d50836321d42599
Horia Bosoanca <horia.bosoanca@gmail.com>
2024-09-22 12:13:42 +0300

    a

--- obj/Debug/net8.0/threads-app.AssemblyInfo.cs
+++ obj/Debug/net8.0/threads-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("threads-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c7e87f28853e87b07c6ce219573d4b1a632979a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f10b4c86055987e11af180891a273eaaf133eff4")]
 [assembly: System.Reflection.AssemblyProductAttribute("threads-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("threads-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
