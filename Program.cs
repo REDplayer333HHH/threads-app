e34ef3c013033c466a32ea29a0a4f874c767c875
Horia Bosoanca <horia.bosoanca@gmail.com>
2024-09-25 10:42:21 +0300

    test

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ using threads_app;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
