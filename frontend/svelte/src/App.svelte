57f7202105c61c3edebeaabf4fe0c9f6ff220479
Horia Bosoanca <horia.bosoanca@gmail.com>
2024-10-10 12:26:19 +0300

    svelte init

---
+++ frontend/svelte/src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+	export let name;
+
+	import { onMount } from "svelte";
+
+	onMount(async () => {
+	fetch("http://localhost:8010/thread")
+	.then(response => response.json())
+	.then(data => {
+		console.log(data);
+	}).catch(error => {
+		console.log(error);
+		return [];
+	});
+	});
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
