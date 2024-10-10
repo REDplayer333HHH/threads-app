158b1fc53a946f88455a7de7935e56045d3cbdb3
Horia Bosoanca <horia.bosoanca@gmail.com>
2024-10-10 12:15:15 +0300

    allow cors

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
 using threads_app;
 
+var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy  =>
+                      {
+                          policy.WithOrigins("http://127.0.0.1:8080");
+                      });
+});
 
 // Add services to the container. 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -23,6 +34,8 @@ app.UseHttpsRedirection();
 
 app.UseStaticFiles();
 app.MapFallbackToFile("index.html");
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapControllers();
 
